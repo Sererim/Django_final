d2741a185ae3162537bd006a8959d105946119c2
Sererim <danielleymanw@gmail.com>
2023-10-20 15:19:04 +0300

    Base commit.

--- django_final/django_final/settings.py
+++ django_final/django_final/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django_fifth.urls'
+ROOT_URLCONF = 'django_final.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django_fifth.wsgi.application'
+WSGI_APPLICATION = 'django_final.wsgi.application'
 
 
 # Database
